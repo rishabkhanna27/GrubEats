a2275385c4926510c8d805a7db04763ed7e08e8a
rishabkhanna27 <rishabkhanna27@gmail.com>
2020-07-08 13:03:35 +0530

    Alignment changes

--- src/component/Home.js
+++ src/component/Home.js
@@ -19,9 +19,15 @@ export class Home extends Component{
 {/* ------------------------------Parallax1--------------------------- */}
                 <div id="start" className="leftbox"> 
                        <p className="Hometext"><p className="Homeheading"><b>Grub<span>Eats</span></b></p>
-                       fkvf  jmkmkmvjdsjlndncdjccdslcnjdnckajcasjcbbjasn dncjnjd<br></br>
-                       chkbsjcshvbjlacvjsnkcbsuvnsdkpbsmc;knregfkewbfjenvbouewnc<br></br>
-                       jsnvnskfjnvjlsdnvlknsdjlvnsjkdnvljsn dk mkddljcvnsdlbskvb</p>
+                       Discover the warm and inviting hospitality that awaits you at GrubEats <br></br>
+                        Hotel Chandigarh, India, with its prime location, luxurious accommodation <br></br>
+                        and elegant finishing touches. Set in Chandigarh's business district, <br></br>
+                        guests are near Sector 17, the airport and the legendary Rock Garden. <br></br>
+                        Our popular restaurants include an in-house microbrewery, international <br></br>
+                        buffet. Host a meeting or special event in one of our versatile <br></br>
+                        conference and wedding venues, including Grand Ballroom and charming <br></br>
+                        lawn venue. Take advantage of our catering and expert <br></br>
+                        planning services to ensure an occasion to remember. </p>
                     </div>
                     <div className="rightbox">
                         <img className="pic" src={image6} alt="px"></img>
@@ -35,10 +41,15 @@ export class Home extends Component{
                       <img className="pic1" src={image7} alt="px"></img>
                   </div>
                     <div className="rightbox1">
-                      <p className="Hometext1"><p className="Homeheading1"><b>From{' '}<span>Our{' '}Chef</span></b></p>
-                        fkvf  jmkmkmvjdsjlndncdjccdslcnjdnckajcasjcbbjasn dncjnjd<br></br>
-                        chkbsjcshvbjlacvjsnkcbsuvnsdkpbsmc;knregfkewbfjenvbouewnc<br></br>
-                        jsnvnskfjnvjlsdnvlknsdjlvnsjkdnvljsn dk mkddljcvnsdlbskvb</p>
+                      <p className="Hometext1"><p className="Homeheading1"><b>Our {' '}<span>Fresh {' '}Chefs</span></b></p>
+                      All Fresh Chef Experience Chefs are college-level culinary school graduates<br></br> 
+                      and working professionals. Our Chef Instructors have a multitude of <br></br>
+                      backgrounds from college-level teaching, to restaurant work, through <br></br>
+                      food research and development. We teach mastering the latest techniques <br></br>
+                      from the basics through advanced skills for all age groups. So, whether <br></br>
+                      you and your guests are beginners desiring to learn some new skills <br></br>
+                      or sophisticated foodies who love to cook, our Fresh Chefs are ready to <br></br>
+                      instruct you on how to combine fresh ideas, food and a lot of fun!</p>
                     </div>
                 </div> 
                 <div className="otherdiv">
@@ -54,9 +65,15 @@ export class Home extends Component{
                 </div>
                 <div className="leftbox2">
                        <p className="Hometext1"><p className="Homeheading1"><b>Save Food{' '}<span>Save Life</span></b></p>
-                       fkvf  jmkmkmvjdsjlndncdjccdslcnjdnckajcasjcbbjasn dncjnjd<br></br>
-                       chkbsjcshvbjlacvjsnkcbsuvnsdkpbsmc;knregfkewbfjenvbouewnc<br></br>
-                       jsnvnskfjnvjlsdnvlknsdjlvnsjkdnvljsn dk mkddljcvnsdlbskvb</p>
+                       Food is a commodity that will soon become a luxury for most of us. Not because <br></br>
+                        we won’t be able to afford it, but because food will not be accessible or <br></br>
+                        available to the masses. Why, you might ask will this happen? It is because <br></br>
+                        the earth too gets exhausted and has an expiry date that may take lifetimes <br></br>
+                        to renew. Back in the days of the French revolution, it was the inability of <br></br>
+                        people to purchase bread that caused social unrest. In the near future, we may <br></br>
+                        not have any food left to fight over. Why? Because we were too caught up with our <br></br>
+                        desires of wanting more for now, that we turned a blind eye to the land that <br></br>
+                        rotted before us. So Lets get together and stop wasting Food</p>
                     </div>
                     </div>
                     <div className="thirdone">
